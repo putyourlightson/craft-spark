ca49c92e925902d7884a22ab768386db85a7165c
Ben Croker <ben@putyourlightson.com>
2024-10-18 15:45:05 +0200

    Return a string

--- src/helpers/SparkUrlHelper.php
+++ src/helpers/SparkUrlHelper.php
@@ -6,31 +6,27 @@
 namespace putyourlightson\spark\helpers;
 
 use Craft;
-use craft\helpers\Template;
 use craft\helpers\UrlHelper;
 use putyourlightson\spark\models\ConfigModel;
 use Twig\Error\SyntaxError;
-use Twig\Markup;
 
 class SparkUrlHelper
 {
-    public static function sparkUrl(string $template, array $variables = [], bool $csrf = false): Markup
+    public static function sparkUrl(string $template, array $variables = [], bool $sendCsrfToken = false): string
     {
         $config = new ConfigModel([
             'siteId' => Craft::$app->getSites()->getCurrentSite()->id,
             'template' => $template,
             'variables' => $variables,
-            'csrf' => $csrf,
+            'sendCsrfToken' => $sendCsrfToken,
         ]);
 
         if (!$config->validate()) {
             throw new SyntaxError(implode(' ', $config->getFirstErrors()));
         }
 
-        $url = UrlHelper::actionUrl('spark/response', [
+        return UrlHelper::actionUrl('spark/response', [
             'config' => $config->getHashed(),
         ]);
-
-        return Template::raw($url);
     }
 }
