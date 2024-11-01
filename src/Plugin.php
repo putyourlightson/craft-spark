4d76fd48907063069bfd7f76736ed1607b1e9fb8
Ben Croker <ben@putyourlightson.com>
2024-11-01 22:03:30 +0100

    MIT license

--- src/Plugin.php
+++ src/Plugin.php
@@ -10,11 +10,6 @@ use putyourlightson\spark\Spark;
 
 class Plugin extends BasePlugin
 {
-    /**
-     * @inerhitdoc
-     */
-    public string $schemaVersion = '0.0.1';
-
     public function init(): void
     {
         parent::init();
